de1c99d11eec0c460a710eac671c7126fad8e607
Kudhan <kudhanshaik01.com>
2025-04-03 18:54:10 +0530

    updated the index.js

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,8 +1,8 @@
 import Hero from './Hero';
 import Footer from './Footer';
 import PopularProducts from './PopularProducts';
-import Services from './services';
-import Subscribe from './subscribe';
+import Services from './Services';
+import Subscribe from './Subscribe';
 import SuperQuality from './SuperQuality';
 import SpecailOffer from './SpecailOffer';
 
