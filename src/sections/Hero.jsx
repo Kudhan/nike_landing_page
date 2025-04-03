eb0348d137ea02923dd7d81a51ef5159ee1a2e88
Kudhan <kudhanshaik01.com>
2025-04-03 18:57:05 +0530

    changes made

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "../components/button";
+import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
 import { shoes, statistics } from "../constants"; // Ensure 'shoes' array is properly imported
 import { bigShoe1 } from "../assets/images";
